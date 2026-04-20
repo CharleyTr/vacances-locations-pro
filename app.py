f2de8119b03a5d2d47df9e13772291821e5728ee
CharleyTr <c.trigano@gmail.com>
2026-04-20 19:55:51 -0400

    Update app.py

--- app.py
+++ app.py
@@ -756,6 +756,7 @@ elif page == "DADS":
             _dads_retry.show()
         except Exception as _e:
             st.error(f"Erreur import DADS : {_e}")
+elif page == "Contrats":       contrats.show()    if contrats    else st.error("Uploadez pages/contrats.py")
 elif page == "Messages":           messages.show()        if messages        else st.error("Uploadez pages/messages.py")
 elif page == "iCal":               ical_sync.show()       if ical_sync       else st.error("Uploadez pages/ical_sync.py")
 elif page == "Propriétés":         _page_proprietes.show() if _page_proprietes else st.error("Uploadez pages/proprietes.py")
